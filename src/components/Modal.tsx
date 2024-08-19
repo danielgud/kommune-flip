1ee6bfc6d6edf6e44479d9f85057e281c1ba72b1
stianmorsund <stian.morsund@ks.no>
2024-08-19 13:19:28 +0200

    modal layout fixes

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,11 +9,9 @@ const Modal = ({ children }: { children: React.ReactNode }) => {
     <dialog
       onCancel={(event) => event.preventDefault()}
       ref={modalRef}
-      className="transition-opacity ease-in-out duration-300 fixed z-20 inset-0 rounded-2xl flex items-center justify-center backdrop:bg-blue-900/80 backdrop:backdrop-blur-md"
+      className="bg-white p-8 shadow-lg w-1/3 min-w-fit transition-opacity ease-in-out duration-300 fixed z-20 inset-0 rounded-2xl flex items-center justify-center backdrop:bg-blue-900/80 backdrop:backdrop-blur-md"
     >
-      <div className="bg-white p-8 rounded-xl shadow-lg w-1/3 min-w-fit">
-        {children}
-      </div>
+      <div className="w-full">{children}</div>
     </dialog>
   );
 };
